04590b97db20a6cbb6eb71785f2b5eaa497701a7
Alex Karpov <keyfour13@gmail.com>
2018-05-24 22:05:58 +0300

    Add abstract BaseActivityView class

---
+++ lib/src/main/java/io/github/keyfour/cola/view/BaseActivityView.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Alexander Karpov <keyfour13@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.keyfour.cola.view;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.github.keyfour.cola.contract.PresenterContract;
+import io.github.keyfour.cola.contract.ViewContract;
+
+/**
+ * @author Alex Karpov <keyfour13@gmail.com> 2018
+ *
+ * MVP View class based on {@link AppCompatActivity}
+ *
+ */
+public abstract class BaseActivityView<V> extends AppCompatActivity implements ViewContract<V> {
+
+    /**
+     * Instance of {@link PresenterContract} to interact with presenter
+     */
+    PresenterContract presenterContract;
+
+    @Override
+    public Context getViewContext() {
+        return this;
+    }
+
+    @Override
+    public abstract void updateView(V model);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenterContract.setView(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (presenterContract != null) {
+            presenterContract.start();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (presenterContract != null) {
+            presenterContract.stop();
+        }
+    }
+
+}
