33ecebbdd43600b39707686839e39b2422a728b7
Alex Karpov <keyfour13@gmail.com>
2018-05-30 08:28:12 +0300

    Add wrapper for shared preferences

---
+++ lib/src/main/java/io/github/keyfour/cola/data/SingleValueStorage.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Alexander Karpov <keyfour13@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.keyfour.cola.data;
+
+/**
+ * @author Alex Karpov <keyfour13@gmail.com> 2018
+ */
+public abstract class SingleValueStorage<V> {
+
+    protected final String filename;
+    protected final String key;
+
+    public SingleValueStorage (String filename, String key) {
+        this.filename = filename;
+        this.key = key;
+    }
+
+    public abstract void put(V value);
+    public abstract V get();
+    public abstract void remove();
+
+}
