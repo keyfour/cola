be7f3a908fa6a6424cd79412d928f9e6d565c29c
Alex Karpov <keyfour13@gmail.com>
2018-05-31 20:30:50 +0300

    Change SharesPreferencesUtils

--- lib/src/main/java/io/github/keyfour/cola/util/SharedPreferencesUtils.java
+++ lib/src/main/java/io/github/keyfour/cola/util/SharedPreferencesUtils.java
@@ -20,83 +20,83 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SharedPreferencesUtils {
 
     public static final String DEFAULT_FILE_NAME = "preferences";
 
-    public static void addString(@NonNull Context context, @NonNull String filename,
-                                 @NonNull String key, @NonNull String value) {
-        getSharedPreferences(context, filename).edit().putString(key,value).apply();
+    public static void putValue(@NonNull Context context, @NonNull String filename,
+                                @NonNull String key, @NonNull Object value) {
+        SharedPreferences.Editor editor = getSharedPreferences(context, filename).edit();
+        put(editor, key, value);
+        editor.apply();
     }
 
-    public static void addString(@NonNull Context context, @NonNull String key,
-                                 @NonNull String value) {
-        addString(context, DEFAULT_FILE_NAME, key, value);
+    public static void putValue(@NonNull Context context, @NonNull String key,
+                                @NonNull Object value) {
+        putValue(context, DEFAULT_FILE_NAME, key, value);
     }
 
-    public static void addInt(@NonNull Context context, @NonNull String filename,
-                              @NonNull String key, @NonNull Integer value) {
-        getSharedPreferences(context, filename).edit().putInt(key, value).apply();
+    public static Object getValue(@NonNull Context context, @NonNull String filename,
+                                @NonNull String key, @NonNull Class clazz) {
+        if (clazz.isInstance(String.class)) {
+            return getSharedPreferences(context, filename).getString(key, "");
+        } else if (clazz.isInstance(Integer.class)) {
+            return getSharedPreferences(context, filename).getInt(key, 0);
+        } else if (clazz.isInstance(Boolean.class)) {
+            return getSharedPreferences(context, filename).getBoolean(key, false);
+        } else if (clazz.isInstance(Float.class)) {
+            return getSharedPreferences(context, filename).getFloat(key, 0.0f);
+        } else if (clazz.isInstance(Long.class)) {
+            return getSharedPreferences(context, filename).getLong(key, 0L);
+        } else if (clazz.isInstance(Set.class)) {
+            return getSharedPreferences(context, filename).getStringSet(key, null);
+        } else {
+            return null;
+        }
     }
 
-    public static void addInt(@NonNull Context context, @NonNull String key,
-                              @NonNull Integer value) {
-        addInt(context, DEFAULT_FILE_NAME, key, value);
+    public static Object getValue(@NonNull Context context, @NonNull String key,
+                                  @NonNull Class clazz) {
+        return getValue(context, DEFAULT_FILE_NAME, key, clazz);
     }
 
-    public static void addBoolean(@NonNull Context context, @NonNull String filename,
-                                  @NonNull String key, @NonNull Boolean value) {
-        getSharedPreferences(context, filename).edit().putBoolean(key, value).apply();
+    public static void putValues(@NonNull Context context, @NonNull String filename,
+                                   @NonNull HashMap<String, Object> hashMap) {
+        SharedPreferences.Editor editor = getSharedPreferences(context, filename).edit();
+        Set<String> keys = hashMap.keySet();
+        for (String key : keys) {
+            put(editor, key, hashMap.get(key));
+        }
+        editor.apply();
     }
 
-    public static void addBoolean(@NonNull Context context, @NonNull String key,
-                                  @NonNull Boolean value) {
-        addBoolean(context, DEFAULT_FILE_NAME, key, value);
-    }
-
-    public static void addFloat(@NonNull Context context, @NonNull String filename,
-                                @NonNull String key, @NonNull Float value) {
-        getSharedPreferences(context, filename).edit().putFloat(key, value).apply();
-    }
-
-    public static void addFloat(@NonNull Context context, @NonNull String key,
-                                @NonNull Float value) {
-        addFloat(context, DEFAULT_FILE_NAME, key, value);
-    }
-
-    public static void addLong(@NonNull Context context, @NonNull String filename,
-                               @NonNull String key, @NonNull Long value) {
-        getSharedPreferences(context, filename).edit().putLong(key, value).apply();
-    }
-
-    public static void addLong(@NonNull Context context, @NonNull String key,
-                               @NonNull Long value) {
-        addLong(context, DEFAULT_FILE_NAME, key, value);
-    }
-
-    public static void addStringSet(@NonNull Context context, @NonNull String filename,
-                                    @NonNull String key, @NonNull Set<String> values) {
-        getSharedPreferences(context, filename).edit().putStringSet(key, values).apply();
-    }
-
-    public static void addStringSet(@NonNull Context context, @NonNull String key,
-                                    @NonNull Set<String> values) {
-        addStringSet(context, DEFAULT_FILE_NAME, key, values);
-    }
-
-    public static void removeObject(@NonNull Context context, @NonNull String filename,
-                                    @NonNull String key) {
-        getSharedPreferences(context, filename).edit().remove(key).apply();
-    }
-
-    public static void removeAllObjects(@NonNull Context context, @NonNull String filename){
-        getSharedPreferences(context, filename).edit().clear().apply();
-    }
-
-    public static void removeAllObjects(Context context) {
-        removeAllObjects(context, DEFAULT_FILE_NAME);
+    private static SharedPreferences.Editor put(@NonNull SharedPreferences.Editor editor,
+                                                @NonNull String key, @NonNull Object value) {
+        if (value instanceof String) {
+            return editor.putString(key, (String) value);
+        } else if (value instanceof Integer) {
+            return  editor.putInt(key, (Integer) value);
+        } else if (value instanceof Boolean) {
+            return editor.putBoolean(key, (Boolean) value);
+        } else if (value instanceof Float) {
+            return editor.putFloat(key, (Float) value);
+        } else if (value instanceof Long) {
+            return editor.putLong(key, (Long) value);
+        } else if (value instanceof Set  && !((Set) value).isEmpty()) {
+            Iterator iterator = ((Set) value).iterator();
+            if (iterator.next() instanceof String) {
+                return editor.putStringSet(key, (Set<String>) value);
+            } else {
+                throw new IllegalArgumentException("Set must consist of String objects");
+            }
+        } else {
+            throw new IllegalArgumentException("Value can be only String, Boolean, Integer, " +
+                    "Float, Long or Set<String>");
+        }
     }
 
     private static SharedPreferences getSharedPreferences(@NonNull Context context,
